f09d34903fa60dd7a29d79ad203c3eb279cc7324
edoRemelli <edoardo.remelli@epfl.ch>
2020-10-14 11:34:12 +0200

    fixes

--- setup.sh
+++ setup.sh
@@ -8,5 +8,10 @@ pip --no-cache-dir install \
 	plyfile \
 	psutil \
 	imageio \
-	neural_renderer_pytorch \
 	--user
+
+mkdir external && cd external
+git clone https://github.com/daniilidis-group/neural_renderer.git && cd neural_renderer
+git fetch origin pull/5cc85740240226b6db9e48d5e5089b4e41681a2c/head:torch15
+git checkout torch15
+python setup.py install --user && cd ../../
