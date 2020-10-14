14fe2c12586515f411fed958ea0ff435c880ebd6
edoRemelli <edoardo.remelli@epfl.ch>
2020-10-14 11:37:36 +0200

    try this

--- setup.sh
+++ setup.sh
@@ -2,16 +2,11 @@ pip install --upgrade pip
 pip --no-cache-dir install \
 	numpy \
 	plotly \
-  torch \
+  torch==1.4.0 \
   torchvision \
 	trimesh \
 	plyfile \
 	psutil \
 	imageio \
+	neural_renderer_pytorch \
 	--user
-
-mkdir external && cd external
-git clone https://github.com/daniilidis-group/neural_renderer.git && cd neural_renderer
-git fetch origin pull/5cc85740240226b6db9e48d5e5089b4e41681a2c/head:torch15
-git checkout torch15
-python setup.py install --user && cd ../../
